ca20e3490de36261937498a0c3712529e6e629d0
Michael Bylstra <mbylstra@gmail.com>
2025-12-03 23:40:24 +1100

    🔧 chore(scripts): update template repository remote URL
    - change remote URL from `mbylstra/flutter-template.git` to `mbylstra/simple-flutter-template.git` to reflect the new repository name

--- scripts/track-template-repo.sh
+++ scripts/track-template-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add the flutter-template repository as a remote named 'template'
-git remote add template git@github.com:mbylstra/flutter-template.git
+git remote add template git@github.com:mbylstra/simple-flutter-template.git
